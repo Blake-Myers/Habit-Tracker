07461ad42e4203a29602f40c4b709ac9f19adf1f
Blake Myers <blakecmyers@gmail.com>
2023-01-30 21:09:44 -0500

    Started Lesson

--- Habit-Tracker/Program.cs
+++ Habit-Tracker/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.Sqlite;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string connectionString = @"Data Source=habit-Tracker.db";
+
+        using (var connection = new SqliteConnection(connectionString))
+        {
+            connection.Open();
+            var tableCmd = connection.CreateCommand();
+
+            tableCmd.CommandText = 
+            @"CREATE TABLE IF NOT EXISTS drinking_water (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                Date TEXT,
+                Quantity INTEGER
+                )";
+
+            tableCmd.ExecuteNonQuery();
+
+            connection.Close();
+            
+        }
+
+    }
+    static void GetUserInput()
+    {
+
+    }
+}
